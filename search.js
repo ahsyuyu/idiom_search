5ce9939e2830290e96bf770dafdca185d6d7133e
ahsyuyu <ahsyuyu@gmail.com>
2014-07-21 13:52:42 +0800

    add text search
    希望可以再改成用兩個輸入來處理搜尋key&def

--- search.js
+++ search.js
@@ -14,6 +14,22 @@ var search=function(tofind){
 	return out;
 }
 
+var search_def=function(tofind){
+	var out=[];
+	//var searchword="tofind";
+	var searchword=new RegExp(tofind,"g");
+
+	for(var i=0;i<idioms.length;i++){
+		var find=idioms[i].def.match(searchword);
+		if(find){
+			idioms[i].def=idioms[i].def.replace(searchword,changecolor);
+			out.push(idioms[i]);
+			//out.replace(searchword_re, changecolor(searchword) );
+		}
+	}
+	return out;
+}
+
 var changecolor=function(input){
 
 	//return "X"+input;
--- ui.js
+++ ui.js
@@ -8,3 +8,10 @@ var dosearch=function(tofind){
 	document.getElementById("result").innerHTML=arr.map(showidiom).join("<br>");
 	document.getElementById("result_num").innerHTML=arr.length+" results";
 }
+
+var dosearch_def=function(tofind){
+	//var tofind=document.getElementById("tofind").value;
+	var arr=search_def(tofind);
+	document.getElementById("result").innerHTML=arr.map(showidiom).join("<br>");
+	document.getElementById("result_num").innerHTML=arr.length+" results";
+}
